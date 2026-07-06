e553dd951e6e5a71897e315d5de9da2bede3ab9c
olivierdekleijn-star <olivierdekleijn@gmail.com>
2026-07-06 22:53:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -36,6 +36,9 @@ app.post("/convert", upload.single("image"), (req, res) => {
 
 });
 
-app.listen(3000, () => {
-    console.log("Server draait op http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server draait op port " + PORT);
+});
 });
