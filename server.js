959f64ee5d9a60dec32ba1c96676e095dfa50647
olivierdekleijn-star <olivierdekleijn@gmail.com>
2026-07-06 22:42:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 
 const app = express();
 app.use(cors());
-app.use(express.static("."));
+app.use(express.static("public"));
 
 const upload = multer({ dest: "uploads/" });
 
